95cf033f3c40b6f67f0e833d36d9caeeb1a575e3
kevin-bingham <kevin@jbingham.net>
2018-05-15 13:31:25 -0700

    print year one

--- cityOfAaron/src/cityOfAaron/CityOfAaron.java
+++ cityOfAaron/src/cityOfAaron/CityOfAaron.java
@@ -7,6 +7,7 @@
 
 package cityOfAaron;
 
+import cityOfAaron.model.CropData;
 import cityOfAaron.model.TeamMember;
 import cityOfAaron.model.Player;
 
@@ -24,9 +25,29 @@ public class CityOfAaron {
         Player playerOne = new Player();
         playerOne.setName("Kevin");
         
+        CropData yearOne = new CropData();
+        yearOne.setYear(1);
+        yearOne.setPopulation(170);
+        yearOne.setAcresOwned(100);
+        yearOne.setCropYield(600);
+        yearOne.setWheatInStore(73);
+        yearOne.setHarvest(510);
+        yearOne.setHarvestAfterOffering(459);
+        yearOne.setNewPeople(25);
+        yearOne.setNumStarved(34);
+        yearOne.setNumberWhoDied(10);
+        yearOne.setOffering(51);
+        yearOne.setOfferingBushels(110);
+        yearOne.setAcresPlanted(90);
+        yearOne.setPeopleFed(134);
+        yearOne.setEatenByRats(89);
+        yearOne.setWheatForPeople(300);
+               
+        
         System.out.println(playerOne.toString());
         System.out.println(TeamMember.Tyler.getName());
         System.out.println(TeamMember.Tyler.getTitle());
+        System.out.println(yearOne.toString());
     }
     
 }
