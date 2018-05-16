e0f8a475d81c42190c8354ec6a4307d50fbba595
kevin-bingham <kevin@jbingham.net>
2018-05-15 18:10:20 -0700

    clean up lesson04

--- cityOfAaron/src/cityOfAaron/model/Player.java
+++ cityOfAaron/src/cityOfAaron/model/Player.java
@@ -47,10 +47,7 @@ public class Player implements Serializable{
             return false;
         }
         final Player other = (Player) obj;
-        if (!Objects.equals(this.name, other.name)) {
-            return false;
-        }
-        return true;
+        return Objects.equals(this.name, other.name);
     }
 
     @Override
