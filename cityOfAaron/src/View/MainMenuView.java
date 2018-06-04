1588f14d3e548ea80f95ee1671417180cac4087f
kevin-bingham <kevin@jbingham.net>
2018-06-04 16:32:56 -0700

    MainMenuView created

---
+++ cityOfAaron/src/View/MainMenuView.java
@@ -0,0 +1,29 @@
+// The MainMenuView class - part of the view layer
+// Object of this class manages the main menu
+// Author: Kevin Bingham and Tyler Day team
+// Date last modified: June 2018
+//-------------------------------------------------------------
+
+package View;
+
+import java.util.Scanner;
+
+public class MainMenuView {
+    Scanner keyboard = new Scanner(System.in);
+    
+    // The displayMenuView method
+    // Purpose: displays the menu, gets the user's input, and does the 
+    //               selected action
+    // Parameters: none
+    // Returns: none
+    // =========================================================
+
+    public void displayMenuView(){
+    // Display the menu
+    // Prompt the user and get the user’s input
+    // Perform the desired action
+    // Determine and display the next view
+
+    }
+
+}
