b454461b8e59dbb7abe72991fa9f7600906ee4f0
kevin-bingham <kevin@jbingham.net>
2018-06-06 12:10:20 -0700

    team assignment 7

--- cityOfAaron/src/View/MainMenuView.java
+++ cityOfAaron/src/View/MainMenuView.java
@@ -1,29 +1,181 @@
-// The MainMenuView class - part of the view layer
-// Object of this class manages the main menu
-// Author: Kevin Bingham and Tyler Day team
-// Date last modified: June 2018
-//-------------------------------------------------------------
+/*
+ * -------------------------------------------------------------
+ * The MainMenuView class - part of the view layer
+ * Object of this class manages the main menu
+ * Author: Kevin Bingham and Tyler Day team
+ * Date last modified: June 2018
+ * -------------------------------------------------------------
+ */
 
 package View;
 
 import java.util.Scanner;
+import cityOfAaron.CityOfAaron;
+import cityOfAaron.model.Player;
+import cityOfAaron.model.Game;
 
 public class MainMenuView {
     Scanner keyboard = new Scanner(System.in);
+    private String theMenu;
+    private int max;
+    
+    // The MainMenuView constructor
+    // Purpose: Initialize the menu data
+    // Parameters: none
+    // Returns: none
+    // ===================================
+    public MainMenuView() {
+        theMenu = "\n" +
+                   "**********************************\n" +
+                   "* CITY OF AARON: MAIN GAME MENU  *\n" +
+                   "**********************************\n" +
+                   " 1 - Start new game\n" +
+                   " 2 - Get and start a saved game\n" +
+                   " 3 - Get help on playing the game\n" +
+                   " 4 - Save game\n" +
+                   " 5 - Quit\n";
+        
+        max = 5;
+    }
     
     // The displayMenuView method
     // Purpose: displays the menu, gets the user's input, and does the 
-    //               selected action
+    //          selected action
     // Parameters: none
     // Returns: none
     // =========================================================
 
-    public void displayMenuView(){
+    public void displayMenuView() {
     // Display the menu
     // Prompt the user and get the user’s input
     // Perform the desired action
     // Determine and display the next view
+    int menuOption = 0;
+    
+        do{
+         // Display the menu
+         System.out.println(theMenu);
+
+         // Prompt the user and get the user’s input
+         menuOption = getMenuOption();
+
+         // Perform the desired action
+         doAction(menuOption);
+
+         // Determine and display the next view
+          } while (menuOption != max);
 
     }
+    
+    // The getMenuOption method
+    // Purpose: gets the user's input
+    // Parameters: none
+    // Returns: integer - the option selected
+    // ===================================       
+    public int getMenuOption(){
+        // declare a variable to hold user’s input
+        int userInput;
 
+        // begin loop
+        do{
+            // get user input from the keyboard
+            userInput = keyboard.nextInt();
+            
+            // if it is not a valid value, output an error message
+            if(userInput < 1 || userInput > max){
+               System.out.println("\noption must be between 1 and " + max);
+            }
+
+        // loop back to the top if input was not valid
+        }while(userInput < 1 || userInput > max);
+
+        // return the value input by the user
+        return userInput;
+    }
+    
+    // The doAction method
+    // Purpose: performs the selected action
+    // Parameters: none
+    // Returns: none
+    // ===================================       
+    public void doAction(int option){
+        switch(option){
+            case 1: // create and start a new game
+                startNewGame();
+                break;
+            case 2: // get and start a saved game
+                startSavedGame();
+                break;
+            case 3: // get help menu
+                displayHelpMenuView();
+                break;
+            case 4: // save game
+                displaySaveGameView();
+                break;
+            case 5:
+                System.out.println("Thanks for playing ... goodbye.");
+        }
+    } 
+    
+     // The startNewGame method
+     // Purpose: creates game object and starts the game
+     // Parameters: none
+     // Returns: none
+     // ===================================     
+     public void startNewGame(){
+        // Create a new Game object.
+        Game theGame = new Game();
+
+        // Save a reference to it in the CityOfAaron class.
+        CityOfAaron.setCurrentGame(theGame);
+        
+        // Display the Banner Page.
+        System.out.println("\nWelcome to the city of Aaron.");
+        
+        // Create a new Player object
+        Player thePlayer = new Player();
+        
+        // Prompt for and get the user’s name.
+        String name;
+        System.out.println("\nPlease type in your first name: ");
+        name = keyboard.next();
+
+        // Save the user’s name in the Player object
+        thePlayer.setName(name);
+
+        // Save a reference to the player object in the Game object
+        theGame.setThePlayer(thePlayer);
+
+        // Display a welcome message
+        System.out.println("\nWelcome " + name + " have fun.");
+
+        // Display the Game menu
+     }
+     
+     // The startSavedGame method
+     // Purpose: get game object and starts saved game
+     // Parameters: none
+     // Returns: none
+     // ===================================     
+     public void startSavedGame(){
+         System.out.println("\nStart saved game option selected.");
+     }
+     
+     // The displayHelpMenuView method
+     // Purpose: opens help menu
+     // Parameters: none
+     // Returns: none
+     // ===================================     
+     public void displayHelpMenuView(){
+         System.out.println("\nHelp option selected.");
+     }
+
+     // The displaySaveGameView method
+     // Purpose: creates game object and starts the game
+     // Parameters: none
+     // Returns: none
+     // ===================================     
+     public void displaySaveGameView(){
+         System.out.println("\nDisplay save game menu.");
+     }
 }
--- cityOfAaron/src/cityOfAaron/CityOfAaron.java
+++ cityOfAaron/src/cityOfAaron/CityOfAaron.java
@@ -1,67 +1,34 @@
 /*
+ * -------------------------------------------------------------
  * The main() class file for the cityOfAaron project
  * CIT-260
  * Spring 2018
  *  Team members: Kevin Bingham, Tyler Day
+ * -------------------------------------------------------------
  */
 
 package cityOfAaron;
 
-import cityOfAaron.model.CropData;
-import cityOfAaron.model.Location;
-import cityOfAaron.model.TeamMember;
-import cityOfAaron.model.Player;
-import cityOfAaron.model.ListItem;
+import View.*;
+import cityOfAaron.model.Game;
 
-/**
- *
- * @author kbingham
- */
 public class CityOfAaron {
+    // variable for keeping a reference to the Game object
+    private static Game theGame = null;
 
-    /**
-     * @param args the command line arguments
-     */
+    public static Game getCurrentGame() {
+        return theGame;
+    }
+
+    public static void setCurrentGame(Game theGame) {
+        CityOfAaron.theGame = theGame;
+    }
+
+    // main function - entry point for the program
+    // runs the main menu
     public static void main(String[] args) {
-        // TODO code application logic here
-        Player playerOne = new Player();
-        playerOne.setName("Kevin");
-        
-        Player playerTwo = new Player();
-        playerTwo.setName("Tyler");
-        
-        CropData yearOne = new CropData();
-        yearOne.setYear(1);
-        yearOne.setPopulation(170);
-        yearOne.setAcresOwned(100);
-        yearOne.setCropYield(600);
-        yearOne.setWheatInStore(73);
-        yearOne.setHarvest(510);
-        yearOne.setHarvestAfterOffering(459);
-        yearOne.setNewPeople(25);
-        yearOne.setNumStarved(34);
-        yearOne.setNumberWhoDied(10);
-        yearOne.setOffering(51);
-        yearOne.setOfferingBushels(110);
-        yearOne.setAcresPlanted(90);
-        yearOne.setPeopleFed(134);
-        yearOne.setEatenByRats(89);
-        yearOne.setWheatForPeople(300);
-        ListItem itemOne = new ListItem();
-        itemOne.setName("Shovel");
-        itemOne.setNumber(1);
-        Location locationOne = new Location();
-        locationOne.setDescription("Castle");
-        locationOne.setSymbol("Image of Castle");
-        
-        System.out.println(playerOne.toString());
-        System.out.println(playerTwo.toString());
-        System.out.println(TeamMember.Tyler.getName());
-        System.out.println(TeamMember.Tyler.getTitle());
-        System.out.println(yearOne.toString());
-        System.out.println(itemOne.toString());
-        System.out.println(locationOne.toString());
-        
+        MainMenuView mmv = new MainMenuView();
+        mmv.displayMenuView();
     }
     
 }
